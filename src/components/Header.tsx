7cf25c4904f962f9f55b3c749c879ea66f2c7a97
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-07 22:41:49 +0800

    feat(ui): style main site title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import Theme from './Theme';
 const Header = () => {
   return (
     <header>
-      <h1>Where in the world?</h1>
+      <h1 className="font-(family-name:--FF) text-[0.865rem] font-extrabold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-150 motion-safe:ease-in-out">
+        Where in the world?
+      </h1>
       <Theme />
     </header>
   );
