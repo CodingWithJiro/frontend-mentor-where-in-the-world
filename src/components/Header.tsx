1627c0ddd60f8ff6fe1e14cad843f69caab41e61
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-07 22:29:14 +0800

    feat(ui): create Header and import Theme and export to App

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Theme from './Theme';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Where in the world?</h1>
+      <Theme />
+    </header>
+  );
+};
+
+export default Header;
