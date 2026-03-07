bd7168ede8daa1bb526df17c7be18f6fd267d781
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-08 00:00:19 +0800

    feat(ui): make Header responsive and align with design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ import Theme from './Theme';
 
 const Header = () => {
   return (
-    <header>
-      <h1 className="font-(family-name:--FF) text-[0.865rem] font-extrabold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-150 motion-safe:ease-in-out">
-        Where in the world?
-      </h1>
-      <Theme />
+    <header className="bg-(--COLOR-BG-CARD-PRIMARY) pt-7.5 pr-2.5 pb-7 pl-4 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[color,background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out md:py-5.5">
+      <div className="mx-auto flex items-center justify-between md:max-w-321 md:pr-1.5">
+        <h1 className="font-(family-name:--FF) text-[clamp(0.875rem,-1rem+3.90625vw,1.5rem)] font-extrabold text-(--COLOR-TEXT-PRIMARY)">
+          Where in the world?
+        </h1>
+        <Theme />
+      </div>
     </header>
   );
 };
