687285e9dce75f6b7b75635aabfd03ec088aced4
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-07 22:43:44 +0800

    feat(ui): remove absolute positioning on Theme

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -6,7 +6,7 @@ const Theme = () => {
   const { theme, handleThemeChange } = useTheme();
 
   return (
-    <div className="absolute top-4 right-4 flex items-start justify-center gap-1 motion-safe:transition-colors motion-safe:duration-150 motion-safe:ease-in-out">
+    <div className="flex items-start justify-center gap-1 motion-safe:transition-colors motion-safe:duration-150 motion-safe:ease-in-out">
       <ThemeIcon theme={theme} />
       <ThemeButton onThemeChange={handleThemeChange} theme={theme} />
     </div>
