cf09da1aa785fd50b60cb362766736de6b88033d
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-09 00:02:40 +0800

    feat(layout): add initial main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import SkipLink from './components/SkipLink';
 import Header from './components/Header';
+import Main from './components/Main';
+import FilterControls from './components/FilterControls';
+import Search from './components/Search';
 
 const App = () => {
   return (
     <>
       <SkipLink />
       <Header />
+      <Main>
+        <FilterControls>
+          <Search />
+        </FilterControls>
+      </Main>
     </>
   );
 };
