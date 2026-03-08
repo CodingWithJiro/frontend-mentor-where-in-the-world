bbd8cca54213399e83274d8da644cfa313fc1fb9
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-09 00:27:11 +0800

    feat(styles): adjust padding in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Theme from './Theme';
 
 const Header = () => {
   return (
-    <header className="bg-(--COLOR-BG-CARD-PRIMARY) pt-7.5 pr-2.5 pb-7 pl-4 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[color,background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out md:py-5.5">
+    <header className="bg-(--COLOR-BG-CARD-PRIMARY) pt-7.5 pr-4 pb-7 pl-4 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[color,background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out md:py-5.5">
       <div className="mx-auto flex items-center justify-between md:max-w-321 md:pr-1.5">
         <h1 className="font-(family-name:--FF) text-[clamp(0.875rem,-1rem+3.90625vw,1.5rem)] font-extrabold text-(--COLOR-TEXT-PRIMARY)">
           Where in the world?
