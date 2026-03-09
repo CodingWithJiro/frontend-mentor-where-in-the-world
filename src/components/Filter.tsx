14fbffc0c10dff90535b3c1d2bfc5a246ddb707b
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-10 00:25:05 +0800

    feat(ui): add HTML structure to Filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,22 @@
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div>
+      <label htmlFor="select-region" className="sr-only">
+        Filter by region.
+      </label>
+
+      <select name="region" id="select-region" value="">
+        <option value="" disabled hidden>
+          Filter by Region
+        </option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </div>
+  );
 };
 
 export default Filter;
