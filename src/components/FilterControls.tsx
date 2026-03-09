a131eaed0c8c19ba91d0837879cbdb8c81f46a02
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-10 00:56:36 +0800

    feat(styles): add gap to FilterControls

--- src/components/FilterControls.tsx
+++ src/components/FilterControls.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 type FilterControlsProps = PropsWithChildren;
 
 const FilterControls = ({ children }: FilterControlsProps) => {
-  return <header className="flex flex-col">{children}</header>;
+  return <header className="flex flex-col gap-10">{children}</header>;
 };
 
 export default FilterControls;
