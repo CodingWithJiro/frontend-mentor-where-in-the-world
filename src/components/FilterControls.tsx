40864060b2b934384f944924354d23a9c282c657
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-11 00:22:38 +0800

    feat(styles): make FilterControls container responsive to larger screens

--- src/components/FilterControls.tsx
+++ src/components/FilterControls.tsx
@@ -3,7 +3,11 @@ import type { PropsWithChildren } from 'react';
 type FilterControlsProps = PropsWithChildren;
 
 const FilterControls = ({ children }: FilterControlsProps) => {
-  return <header className="flex flex-col gap-10">{children}</header>;
+  return (
+    <header className="flex flex-col gap-10 md:mx-auto md:max-w-7xl md:flex-row md:justify-between">
+      {children}
+    </header>
+  );
 };
 
 export default FilterControls;
