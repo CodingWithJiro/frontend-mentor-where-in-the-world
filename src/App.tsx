f73e19eea062bb0a7e685938bda0191f5f2b1b6c
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-13 01:43:42 +0800

    refactor(ui): remove hook logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import SkipLink from './components/SkipLink';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -9,29 +8,6 @@ import Countries from './components/Countries';
 import CountryList from './components/CountryList';
 
 const App = () => {
-  const [countries, setCountries] = useState(null);
-
-  useEffect(() => {
-    const fetchAllCountries = async () => {
-      try {
-        const response = await fetch(
-          'https://restcountries.com/v3.1/all?fields=name,capital,currencies',
-        );
-        const data = await response.json();
-        setCountries(data);
-      } catch (err) {
-        if (err instanceof Error) {
-          throw Error(
-            `Unable to fetch data from REST countries API: ${err.message}`,
-          );
-        } else {
-          throw Error(`An unknown error occured:`);
-        }
-      }
-    };
-    fetchAllCountries();
-  }, []);
-
   return (
     <>
       <SkipLink />
@@ -42,7 +18,7 @@ const App = () => {
           <Filter />
         </FilterControls>
         <Countries>
-          <CountryList countries={countries} />
+          <CountryList />
         </Countries>
       </Main>
     </>
