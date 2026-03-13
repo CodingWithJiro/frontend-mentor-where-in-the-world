ba28318b0851733c3797c5164cd95eca73d63064
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-14 03:17:32 +0800

    feat(api): fetch country details from api using countryCode

--- src/services/countriesApi.ts
+++ src/services/countriesApi.ts
@@ -1,5 +1,6 @@
-import type { CountryListData } from '../types/country';
+import type { CountryListData, CountryDetails } from '../types/country';
 import { getFormattedCountryListData } from '../utils/formatCountryData';
+import { getFormattedCountryDetails } from '../utils/formatCountryDetails';
 
 export const fetchCountryListData = async () => {
   const response = await fetch(
@@ -10,5 +11,17 @@ export const fetchCountryListData = async () => {
 
   const data: CountryListData[] = await response.json();
   const formattedData = getFormattedCountryListData(data);
+  return formattedData;
+};
+
+export const fetchCountryDetails = async (countryCode: string) => {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/alpha/${countryCode}?fields=name,subregion,tld,currencies,languages,borders`,
+  );
+
+  if (!response.ok) throw new Error('Failed to fetch country details.');
+
+  const data: CountryDetails = await response.json();
+  const formattedData = getFormattedCountryDetails(data);
   return formattedData;
 };
