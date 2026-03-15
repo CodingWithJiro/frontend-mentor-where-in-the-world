a852c79f2aa06afb272dff6ead7d0a45f657701e
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-16 01:40:32 +0800

    feat(layout): make Countries grid and add spacings

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -3,7 +3,11 @@ import type { PropsWithChildren } from 'react';
 type CountriesProps = PropsWithChildren;
 
 const Countries = ({ children }: CountriesProps) => {
-  return <ul>{children}</ul>;
+  return (
+    <ul className="grid grid-cols-1 justify-items-center gap-y-10">
+      {children}
+    </ul>
+  );
 };
 
 export default Countries;
