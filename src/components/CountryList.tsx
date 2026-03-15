77e106f6d8a88e90a0ff6e0949b524ab02eebc8e
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-16 01:41:19 +0800

    feat(styles): customize CountryList style

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -7,7 +7,10 @@ type CountryListProps = {
 
 const CountryList = ({ countryList }: CountryListProps) => {
   return countryList.map((country) => (
-    <li key={country.countryCode}>
+    <li
+      key={country.countryCode}
+      className="max-w-66 min-w-66 overflow-hidden rounded-sm bg-(--COLOR-BG-CARD-PRIMARY) shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out"
+    >
       <CountryCard {...country} />
     </li>
   ));
