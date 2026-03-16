97dc361508fee420203f041824b218367cba34aa
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-17 00:45:27 +0800

    feat(ui): make Countries grid responsive on screen size and adjust spacings

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -4,7 +4,7 @@ type CountriesProps = PropsWithChildren;
 
 const Countries = ({ children }: CountriesProps) => {
   return (
-    <ul className="grid grid-cols-1 justify-items-center gap-y-10">
+    <ul className="mx-auto grid max-w-160 grid-cols-1 justify-items-center gap-y-10 sm:grid-cols-2 sm:gap-y-18.5 lg:max-w-250 lg:grid-cols-3 xl:max-w-338 xl:grid-cols-4">
       {children}
     </ul>
   );
