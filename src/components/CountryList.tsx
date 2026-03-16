6eb7ea23e4e8e0abb9c9499378a464aeec39c427
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-17 02:02:43 +0800

    feat(ui): update prop to filteredCountries in CountryList

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -2,11 +2,11 @@ import CountryCard from './CountryCard';
 import type { FormattedCountryListData } from '../types/country';
 
 type CountryListProps = {
-  countryList: FormattedCountryListData[];
+  filteredCountries: FormattedCountryListData[];
 };
 
-const CountryList = ({ countryList }: CountryListProps) => {
-  return countryList.map((country) => (
+const CountryList = ({ filteredCountries }: CountryListProps) => {
+  return filteredCountries.map((country) => (
     <li
       key={country.countryCode}
       className="max-w-66 min-w-66 overflow-hidden rounded-sm bg-(--COLOR-BG-CARD-PRIMARY) shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out"
