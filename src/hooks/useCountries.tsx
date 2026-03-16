fd9d937be4264100a62bb18ce0ac7fd9079206a2
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-17 01:56:34 +0800

    feat(hooks): update useCountries to integrate search feature

--- src/hooks/useCountries.tsx
+++ src/hooks/useCountries.tsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { fetchCountryListData } from '../services/countriesApi';
+import { getFormattedQuery } from '../utils/formatQuery';
 import type { FormattedCountryListData } from '../types/country';
 
 const useCountries = () => {
   const [countryList, setCountryList] = useState<FormattedCountryListData[]>(
     [],
   );
+  const [query, setQuery] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -23,7 +25,21 @@ const useCountries = () => {
     loadCountryList();
   }, []);
 
-  return { countryList, error };
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const formattedQuery = getFormattedQuery(query);
+  const filteredCountries = useMemo(() => {
+    if (!formattedQuery) return countryList;
+
+    return countryList.filter((country) => {
+      const countryLowerCase = country.name.toLowerCase();
+      return countryLowerCase.includes(formattedQuery);
+    });
+  }, [formattedQuery, countryList]);
+
+  return { filteredCountries, error, query, handleSearch };
 };
 
 export default useCountries;
