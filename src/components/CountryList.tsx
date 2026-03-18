4bdc249efeba8a27d4c15b8fd32ac5d36d8caa74
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-19 01:21:51 +0800

    feat(styles): add animation to list elements in CountryList

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -8,8 +8,8 @@ type CountryListProps = {
 const CountryList = ({ filteredCountries }: CountryListProps) => {
   return filteredCountries.map((country) => (
     <li
-      key={country.countryCode}
-      className="max-w-66 min-w-66 overflow-hidden rounded-sm bg-(--COLOR-BG-CARD-PRIMARY) shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out"
+      key={country.countryCode + country.region}
+      className="fade-in max-w-66 min-w-66 overflow-hidden rounded-sm bg-(--COLOR-BG-CARD-PRIMARY) shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out"
     >
       <CountryCard {...country} />
     </li>
