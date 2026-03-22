b62614d64f5b5c87db9a5bfd50647390516a4e2e
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-23 01:13:33 +0800

    refactor: implement RouterProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
-import SkipLink from './components/SkipLink';
-import Header from './components/Header';
-import Main from './components/Main';
-import FilterControls from './components/FilterControls';
-import Search from './components/Search';
-import Filter from './components/Filter';
-import Countries from './components/Countries';
-import CountryList from './components/CountryList';
-import useCountries from './hooks/useCountries';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
 
 const App = () => {
-  const {
-    filteredCountries,
-    isEmpty,
-    query,
-    handleSearch,
-    isFilterOpen,
-    handleFilterOpen,
-    region,
-    handleSelectRegion,
-    status,
-  } = useCountries();
-  return (
-    <>
-      <SkipLink />
-      <Header />
-      <Main>
-        <FilterControls>
-          <Search query={query} onSearch={handleSearch} />
-          <Filter
-            isFilterOpen={isFilterOpen}
-            onFilterOpen={handleFilterOpen}
-            region={region}
-            onSelectRegion={handleSelectRegion}
-          />
-        </FilterControls>
-        <Countries status={status} isEmpty={isEmpty}>
-          <CountryList filteredCountries={filteredCountries} />
-        </Countries>
-      </Main>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
