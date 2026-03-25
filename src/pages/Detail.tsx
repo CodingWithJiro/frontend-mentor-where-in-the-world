9eb9ec17f798fde3d87ba8fafc34ed0eb4debbbc
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-26 00:25:11 +0800

    feat(pages): update Details to show country details data from API

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,59 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fetchCountryDetails } from '../services/countriesApi';
+import type { FormattedCountryDetails, Status } from '../types/country';
+
 const Detail = () => {
-  return <div>Detail</div>;
+  const { code } = useParams();
+  const [country, setCountry] = useState<FormattedCountryDetails | null>(null);
+  const [status, setStatus] = useState<Status>('loading');
+
+  useEffect(() => {
+    if (!code) return;
+
+    const loadDetails = async () => {
+      setStatus('loading');
+
+      try {
+        const data = await fetchCountryDetails(code);
+        setCountry(data);
+        setStatus('success');
+      } catch {
+        setStatus('fail');
+      }
+    };
+
+    loadDetails();
+  }, [code]);
+
+  if (status === 'loading') return <p>Loading country details...</p>;
+  if (status === 'fail') return <p>Fetching country details failed.</p>;
+
+  if (country) {
+    return (
+      <section>
+        <img src={country.flagImage} alt={country.flagAlt} />
+        <h2>Name: {country.name}</h2>
+        <p>Native name: {country.nativeName}</p>
+        <p>Population: {country.population.toLocaleString()}</p>
+        <p>Region: {country.region}</p>
+        <p>Sub Region: {country.subregion}</p>
+        <p>Capital: {country.capital}</p>
+        <p>Top Level Domain: {country.tld.join(', ')}</p>
+        <p>Currencies: {country.currencies.join(', ')}</p>
+        <p>Languages: {country.languages.join(', ')}</p>
+        <p>Border Countries:</p>
+        {country.borderNames && (
+          <ul>
+            {country.borderNames.map((border) => (
+              <li key={border.cca3}>{border.name}</li>
+            ))}
+          </ul>
+        )}
+        {!country.borderNames && <p>No border countries</p>}
+      </section>
+    );
+  }
 };
 
 export default Detail;
