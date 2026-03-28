23cc89d8cff296814bafb537cf1f4efdb5d79632
CodingWithJiro <chavezelmar03@gmail.com>
2026-03-29 02:37:52 +0800

    feat(ux): add focus outline state to site title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <header className="mb-5.75 bg-(--COLOR-BG-CARD-PRIMARY) pt-7.5 pr-4 pb-6.75 pl-4 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-[color,background-color,box-shadow] motion-safe:duration-150 motion-safe:ease-in-out md:mb-12 md:py-5.5">
       <div className="mx-auto flex items-center justify-between md:max-w-7xl">
-        <Link to="/">
+        <Link
+          to="/"
+          className="rounded-sm focus-visible:outline focus-visible:outline-offset-6 focus-visible:outline-(--COLOR-OUTLINE-PRIMARY)"
+        >
           <h1 className="font-(family-name:--FF) text-[clamp(0.875rem,-1rem+3.90625vw,1.5rem)] font-extrabold text-(--COLOR-TEXT-PRIMARY)">
             Where in the world?
           </h1>
