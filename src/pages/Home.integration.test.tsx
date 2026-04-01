a4ff1ca21e8fc4673cdadf960129ac0a6a75047e
CodingWithJiro <chavezelmar03@gmail.com>
2026-04-02 00:30:28 +0800

    test(home): filters and displays countries on user search

--- src/pages/Home.integration.test.tsx
+++ src/pages/Home.integration.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
+import userEvent from '@testing-library/user-event';
 
 describe('Home Page Integration Tests', () => {
   test('shows loading state, then renders countries', async () => {
@@ -21,6 +22,27 @@ describe('Home Page Integration Tests', () => {
 
       const loadingMessage = screen.queryByText(/loading countries/i);
       expect(loadingMessage).not.toBeInTheDocument();
+    });
+  });
+
+  test('filters and displays countries on user search', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+
+    const searchInput = await screen.findByRole('searchbox');
+    await user.type(searchInput, 'phi');
+
+    const phCard = await screen.findByRole('link', { name: /philippines/i });
+    expect(phCard).toBeInTheDocument();
+
+    await waitFor(() => {
+      const jpCard = screen.queryByRole('link', { name: /japan/i });
+      expect(jpCard).not.toBeInTheDocument();
     });
   });
 });
