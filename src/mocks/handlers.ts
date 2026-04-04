3425327c341d1822e2c86b52b0ef95efafc6f7e9
CodingWithJiro <chavezelmar03@gmail.com>
2026-04-05 00:47:16 +0800

    test(mocks): add more api handlers for country details

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -44,4 +44,40 @@ export const handlers = [
       },
     ]);
   }),
+  http.get('https://restcountries.com/v3.1/alpha/:code', () => {
+    return HttpResponse.json({
+      name: { common: 'Japan', nativeName: { jpn: { common: '日本' } } },
+      subregion: 'Eastern Asia',
+      tld: ['.jp', '.みんな'],
+      currencies: {
+        JPY: { name: 'Japanese yen' },
+        USD: { name: 'US Dollar' },
+      },
+      languages: {
+        jpn: 'Japanese',
+        eng: 'English',
+      },
+      borders: ['CHN', 'KOR'],
+      flags: {
+        svg: 'https://flagcdn.com/jp.svg',
+        alt: 'The flag of Japan',
+      },
+      population: 125_800_000,
+      region: 'Asia',
+      capital: ['Tokyo'],
+    });
+  }),
+  http.get('https://restcountries.com/v3.1/alpha', ({ request }) => {
+    const url = new URL(request.url);
+    const codes = url.searchParams.get('codes');
+
+    if (codes === 'CHN,KOR') {
+      return HttpResponse.json([
+        { name: { common: 'China' }, cca3: 'CHN' },
+        { name: { common: 'South Korea' }, cca3: 'KOR' },
+      ]);
+    }
+
+    return HttpResponse.json([]);
+  }),
 ];
