87ac3f7682dfcb697052e23edb007b8164f47f30
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-01 00:37:15 +0800

    refactor(test): update country borders formatting

--- src/utils/formatCountryBorders.test.ts
+++ src/utils/formatCountryBorders.test.ts
@@ -1,27 +1,35 @@
 import getFormattedCountryBorders from './formatCountryBorders';
+
 import type { CountryBorders } from '../types/country';
 
 describe('getFormattedCountryBorders', () => {
   test('formats country borders correctly', () => {
     const mockData: CountryBorders[] = [
       {
-        name: {
+        names: {
           common: 'Philippines',
         },
-        cca3: 'PHL',
+        codes: {
+          alpha_3: 'PHL',
+        },
       },
       {
-        name: {
+        names: {
           common: 'Japan',
         },
-        cca3: 'JPN',
+        codes: {
+          alpha_3: 'JPN',
+        },
       },
     ];
+
     const result = getFormattedCountryBorders(mockData);
+
     const expectedResult = [
       { name: 'Philippines', cca3: 'PHL' },
       { name: 'Japan', cca3: 'JPN' },
     ];
+
     expect(result).toEqual(expectedResult);
   });
 });
